c69349ac4275baf835ec2888343d0c3fd15a4a80
wudaming <15201555981@163.com>
2018-04-16 18:20:21 +0800

    调整结构

--- nestedscrolling/src/main/java/com/arise/demo/nestedscrolling/LinkedChild.kt
+++ nestedscrolling/src/main/java/com/arise/demo/nestedscrolling/LinkedChild.kt
@@ -17,7 +17,7 @@ interface LinkedChild {
 
     /**
      * @param x 可用的x轴距离
-     * @param y 可用的y轴距离
+     * @param y 可用的y轴距离,y>0表示手指向上滑动了一段距离
      * @param consumed 用于回传消息，传进来时一定是二维数组[0,0]
      */
     fun onLinkedScroll(x: Int, y: Int, consumed: IntArray)
--- nestedscrolling/src/main/java/com/arise/demo/nestedscrolling/NestedScrollingParent2View.kt
+++ nestedscrolling/src/main/java/com/arise/demo/nestedscrolling/NestedScrollingParent2View.kt
@@ -6,23 +6,42 @@ import android.support.v4.view.ViewCompat
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
+import android.view.VelocityTracker
 import android.view.View
+import android.view.ViewConfiguration
 import android.widget.LinearLayout
+import android.widget.OverScroller
 
 /**
  * 嵌套滑动机制内置实现版本
  */
-class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2 {
+class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2, Runnable {
 
     private val INVALID_POINTER = -1
 
     private var mLastTouchX = 0
     private var mLastTouchY = 0
 
+    private var mLastScrollX = 0
+    private var mLastScrollY = 0
+
     private var mScrollPointerId = INVALID_POINTER
+
+    private val mVelocityTracker = VelocityTracker.obtain()
+
+    private lateinit var scroller: OverScroller
+
+    private lateinit var viewConfig: ViewConfiguration
+
+    private val TAG = NestedScrollingParent2View::class.java.simpleName
+
+    private val linkedChildren = ArrayList<LinkedChild>()
+
+    var scrollingView: View? = null
 
 
     override fun onStopNestedScroll(target: View, type: Int) {
+        dispatchStop()
     }
 
     override fun onNestedScrollAccepted(child: View, target: View, axes: Int, type: Int) {
@@ -32,11 +51,23 @@ class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2 {
     override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int) {
     }
 
-    private val TAG = NestedScrollingParent2View::class.java.simpleName
+    override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
+        return (axes and ViewCompat.SCROLL_AXIS_VERTICAL) != 0
+    }
 
-    private val linkedChildren = ArrayList<LinkedChild>()
+    override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray?, type: Int) {
 
-    var scrollingView: View? = null
+        if (target.canScrollVertically(-1) && dy < 0) {
+            return
+        }
+        val safeConsumed = IntArray(2)
+        dispatchToChildren(dx, dy, safeConsumed)
+        if (consumed != null) {
+            consumed[0] = safeConsumed[0]
+            consumed[1] = safeConsumed[1]
+        }
+
+    }
 
     constructor(context: Context) : this(context, null)
 
@@ -48,12 +79,16 @@ class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2 {
 
     private fun init(context: Context) {
         findLinkScrollChildren()
+        scroller = OverScroller(context)
+        viewConfig = ViewConfiguration.get(context)
 
     }
 
     override fun onTouchEvent(e: MotionEvent): Boolean {
         val action = e.actionMasked
         val actionIndex = e.actionIndex
+        val vtev = MotionEvent.obtain(e)
+        var eventAddedToVelocityTracker = false
         when (action) {
             MotionEvent.ACTION_DOWN -> {
                 mScrollPointerId = e.getPointerId(0)
@@ -77,15 +112,9 @@ class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2 {
 
                 val x = (e.getX(index) + 0.5f).toInt()
                 val y = (e.getY(index) + 0.5f).toInt()
-                var dx = mLastTouchX - x
-                var dy = mLastTouchY - y
-                val mScrollConsumed = IntArray(2)
-                dispatchInternal(dx, dy, mScrollConsumed)
-                dx -= mScrollConsumed[0]
-                dy -= mScrollConsumed[1]
-                if (dx != 0 || dy != 0) {
-                    scrollingView?.apply { this.scrollBy(dx, dy) }
-                }
+                val dx = mLastTouchX - x
+                val dy = mLastTouchY - y
+                dispatchInternal(dx, dy)
                 mLastTouchX = x
                 mLastTouchY = y
             }
@@ -93,12 +122,26 @@ class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2 {
             MotionEvent.ACTION_POINTER_UP -> onPointerUp(e)
 
             MotionEvent.ACTION_UP -> {
+                mVelocityTracker.addMovement(vtev)
+                eventAddedToVelocityTracker = true
+                mVelocityTracker.computeCurrentVelocity(1000, viewConfig.scaledMaximumFlingVelocity.toFloat())
+
+                scroller.fling(0, 0, 0, getVelocityY()
+                        , Integer.MIN_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MAX_VALUE)
+                postOnAnimation(this)
                 dispatchStop()
+                mVelocityTracker.clear()
             }
-            MotionEvent.ACTION_CANCEL -> dispatchStop()
+            MotionEvent.ACTION_CANCEL -> {
+                dispatchStop()
+                mVelocityTracker.clear()
+            }
 
         }
-
+        if (!eventAddedToVelocityTracker) {
+            mVelocityTracker.addMovement(vtev)
+        }
+        vtev.recycle()
         return true
     }
 
@@ -112,34 +155,6 @@ class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2 {
             mLastTouchY = (e.getY(newIndex) + 0.5f).toInt()
         }
     }
-
-    private fun dispatchStop() {
-        if (linkedChildren.isNotEmpty()) {
-            linkedChildren.forEach {
-                it.onStopLinkedScroll()
-            }
-        }
-    }
-
-    override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
-        return (axes and ViewCompat.SCROLL_AXIS_VERTICAL) != 0
-    }
-
-
-    override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray?, type: Int) {
-
-        if (target.canScrollVertically(-1) && dy < 0) {
-            return
-        }
-        val safeConsumed = IntArray(2)
-        dispatchInternal(dx, dy, safeConsumed)
-        if (consumed != null) {
-            consumed[0] = safeConsumed[0]
-            consumed[1] = safeConsumed[1]
-        }
-
-    }
-
 
     override fun onViewAdded(child: View) {
         super.onViewAdded(child)
@@ -165,11 +180,16 @@ class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2 {
         }
     }
 
-    private fun dispatchInternal(dx: Int, dy: Int, consumed: IntArray) {
-        val childrenConsume = intArrayOf(0, 0)
-        dispatchToChildren(dx, dy, childrenConsume)
-        consumed[0] += childrenConsume[0]
-        consumed[1] += childrenConsume[1]
+    private fun dispatchInternal(dx: Int, dy: Int) {
+        var dx1 = dx
+        var dy1 = dy
+        val mScrollConsumed = IntArray(2)
+        dispatchToChildren(dx, dy, mScrollConsumed)
+        dx1 -= mScrollConsumed[0]
+        dy1 -= mScrollConsumed[1]
+        if (dx1 != 0 || dy1 != 0) {
+            scrollingView?.apply { this.scrollBy(dx1, dy1) }
+        }
     }
 
     private fun dispatchToChildren(dx: Int, dy: Int, consumed: IntArray) {
@@ -193,11 +213,51 @@ class NestedScrollingParent2View : LinearLayout, NestedScrollingParent2 {
 
     }
 
+    private fun dispatchStop() {
+        if (linkedChildren.isNotEmpty()) {
+            linkedChildren.forEach {
+                it.onStopLinkedScroll()
+            }
+        }
+    }
+
     private fun findLinkScrollChildren() {
         (0..(childCount - 1))
                 .map { getChildAt(it) }
                 .filterIsInstance<LinkedChild>()
                 .filterTo(linkedChildren) { it.enableLinkedScroll }
+    }
+
+    override fun onDetachedFromWindow() {
+        mVelocityTracker.recycle()
+        super.onDetachedFromWindow()
+    }
+
+    override fun run() {
+        if (scroller.computeScrollOffset()) {
+            val x = scroller.currX
+            val y = scroller.currY
+            Log.e("wdm", "last($mLastScrollX,$mLastScrollY)==curr($x,$y)")
+            dispatchInternal(mLastScrollX - x, mLastScrollY - y)
+            mLastScrollX = x
+            mLastScrollY = y
+
+            postOnAnimation(this)
+        }
+    }
+
+    private fun getVelocityY(): Int {
+        var velocityY = 0
+        val rawVelocityY = mVelocityTracker.getYVelocity(mScrollPointerId)
+        velocityY = if (Math.abs(rawVelocityY) < viewConfig.scaledMinimumFlingVelocity) {
+            0
+        } else {
+            Math.max(-viewConfig.scaledMaximumFlingVelocity,
+                    Math.min(velocityY, viewConfig.scaledMaximumFlingVelocity))
+        }
+
+
+        return velocityY
     }
 
 }
