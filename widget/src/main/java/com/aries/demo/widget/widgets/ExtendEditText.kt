f0ef0cced3dc5252ce47dfc908004f600f68d408
wudaming <15201555981@163.com>
2018-06-04 11:22:29 +0800

    icon的显示控制

--- widget/src/main/java/com/aries/demo/widget/widgets/ExtendEditText.kt
+++ widget/src/main/java/com/aries/demo/widget/widgets/ExtendEditText.kt
@@ -1,9 +1,12 @@
 package com.aries.demo.widget.widgets
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.text.method.PasswordTransformationMethod
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.RelativeLayout
 import com.aries.demo.widget.R
 import kotlinx.android.synthetic.main.layout_extend_edit_text.view.*
@@ -12,9 +15,12 @@ import kotlinx.android.synthetic.main.layout_extend_edit_text.view.*
  * Author wudaming
  * Created on 2018/5/29
  */
-class ExtendEditText : RelativeLayout {
+class ExtendEditText : RelativeLayout, TextWatcher {
 
     private var isChecked = false
+    private var hasFocus = false
+    private var isEmpty = true
+    private var isPassword = false
 
     constructor(context: Context) : super(context) {
         init(context)
@@ -51,7 +57,39 @@ class ExtendEditText : RelativeLayout {
                 secondIcon.setImageResource(R.drawable.edit_text_show)
             }
         }
+
+        mEditText.setOnFocusChangeListener { _, hasFocus ->
+            this.hasFocus = hasFocus
+            refreshRightIcons()
+        }
+
+        mEditText.addTextChangedListener(this)
     }
 
+    fun addTextChangedListener(watcher: TextWatcher) {
+        mEditText.addTextChangedListener(watcher)
+    }
 
+    fun removeTextChangedListener(watcher: TextWatcher) {
+        mEditText.removeTextChangedListener(watcher)
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        isEmpty = s.isNullOrEmpty()
+        refreshRightIcons()
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+    }
+
+    private fun refreshRightIcons() {
+        firstIcon.visibility = if (!isEmpty && hasFocus) View.VISIBLE else View.GONE
+        secondIcon.visibility = if (!isEmpty && hasFocus && isPassword) View.VISIBLE
+        else View.GONE
+
+    }
 }
